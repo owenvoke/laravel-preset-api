fc6291c733920fa4fe01bdf2507de1590a76a0b1
Owen Voke <owzie123@gmail.com>
2019-07-05 15:02:21 +0100

    Initial commit

---
+++ src/LaravelApiPresetServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace pxgamer\LaravelApiPreset;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Foundation\Console\PresetCommand;
+
+final class LaravelApiPresetServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        //
+    }
+}
