b778d2cc40d53119da3910b7e76e1ab8f2f48f44
Owen Voke <owzie123@gmail.com>
2019-07-05 15:24:11 +0100

    Add the preset install to the service provider

--- src/LaravelApiPresetServiceProvider.php
+++ src/LaravelApiPresetServiceProvider.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace pxgamer\LaravelApiPreset;
 
+use Illuminate\Console\Command;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Foundation\Console\PresetCommand;
 
@@ -11,6 +12,10 @@ final class LaravelApiPresetServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        //
+        PresetCommand::macro('api', function (Command $command): void {
+            LaravelApiPreset::install();
+
+            $command->info('Frontend scaffolding removed successfully.');
+        });
     }
 }
