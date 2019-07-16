1fee37acbea12231fa80d6c6e555f702fd1b9733
Owen Voke <owzie123@gmail.com>
2019-07-16 09:37:33 +0100

    Add command warning message and confirmation

--- src/LaravelApiPresetServiceProvider.php
+++ src/LaravelApiPresetServiceProvider.php
@@ -13,9 +13,17 @@ final class LaravelApiPresetServiceProvider extends ServiceProvider
     public function boot(): void
     {
         PresetCommand::macro('api', function (Command $command): void {
+            $command->warn('This command will remove all frontend files (i.e. package.json, JavaScript, SASS, views, etc.)');
+
+            if (! $command->confirm('Are you sure you want to install this preset?')) {
+                $command->info('Command aborted successfully');
+
+                return;
+            }
+
             LaravelApiPreset::install();
 
-            $command->info('Frontend scaffolding removed successfully.');
+            $command->info('Frontend scaffolding removed successfully');
         });
     }
 }
